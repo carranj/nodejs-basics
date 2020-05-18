d3313d167bd85f7d2a0db7c6ad5d8c23155217e9
Jose Carranco <josecarranco@Joses-MacBook-Pro.local>
2020-05-17 23:27:59 -0500

    Initialize and plan the project

---
+++ app.js
@@ -0,0 +1,7 @@
+//Problem: We need a simple way to look at a user's badge count and JavaScript points
+//Solution: Use node.js to connect to Treehouse's API to get profile information to print out
+
+//connect to the API URL (https://teamtreehouse.com/username.json)
+//Read the data
+//Parse the data
+//Print the data
