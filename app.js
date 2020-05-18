396c4c87b88f02760325018b7b00512ff343b57f
Jose Carranco <jcarranco@tvi-mp3.com>
2020-05-18 10:36:33 -0500

    Parsing JSON

--- app.js
+++ app.js
@@ -11,8 +11,16 @@ function printMessage(username, badgecount, points){
 
 //connect to the API URL (https://teamtreehouse.com/username.json)
 const request = https.get(`https://teamtreehouse.com/${username}.json`, response =>{
+    let body = "";    
     //Read the data
-    console.log(response.statusCode);
-    //Parse the data
-    //Print the data
+    response.on('data', data => {
+        body += data.toString();
+    });
+
+    response.on('end', ()=>{
+        //Parse the data
+        const profile = JSON.parse(body);
+        //Print the data
+        printMessage(username, profile.badges.length, profile.points.JavaScript)
+    });    
 });
