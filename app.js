88016c1e517a88cb520783f84860798bcdcb511e
Jose Carranco <josecarranco@Joses-MacBook-Pro.local>
2020-05-17 23:44:42 -0500

    Making a GET Request with https

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 //Problem: We need a simple way to look at a user's badge count and JavaScript points
 //Solution: Use node.js to connect to Treehouse's API to get profile information to print out
+const https = require('https');
+const username = 'williamallen4';
+
+//Function to print message to console
+function printMessage(username, badgecount, points){
+    const message = `${username} has ${badgecount} total badge(s) and ${points} points in JavaScript`
+    console.log(message)
+}
 
 //connect to the API URL (https://teamtreehouse.com/username.json)
-//Read the data
-//Parse the data
-//Print the data
+const request = https.get(`https://teamtreehouse.com/${username}.json`, response =>{
+    //Read the data
+    console.log(response.statusCode);
+    //Parse the data
+    //Print the data
+});
